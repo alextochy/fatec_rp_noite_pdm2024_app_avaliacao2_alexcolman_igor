6ca44147cdf1f5c5e1cd1a2292006692192f8525
Alexcolman Tochukwu Apunike <alexcolman.apunike@gmail.com>
2024-05-26 22:26:23 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fatec_rp_noite_pdm2024_app_avaliacao2_alexcolman_igor
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fatec_rp_noite_pdm2024_app_avaliacao2_alexcolman_igor/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fatec_rp_noite_pdm2024_app_avaliacao2_alexcolman_igor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
